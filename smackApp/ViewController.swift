8143a203bb0f0e9232b5b2601618f1a2c0d7577a
Huy Vu <Huy.Vu@searshc.com>
2018-10-08 20:38:00 -0500

    Initial Commit

---
+++ smackApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smackApp
+//
+//  Created by Vu, Huy (Contractor) on 10/8/18.
+//  Copyright © 2018 Vu, Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
