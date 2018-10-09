791bdfb30c2c61e076ce56c93316e1b8f33e70ea
Huy Vu <Huy.Vu@searshc.com>
2018-10-08 21:26:41 -0500

    add reveal

--- smackApp/Controller/ChannelVC.swift
+++ smackApp/Controller/ChannelVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChannelVC.swift
 //  smackApp
 //
 //  Created by Vu, Huy (Contractor) on 10/8/18.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.revealViewController()?.rearViewRevealWidth = self.view.bounds.width - 60
     }
-
-
 }
-
---
+++ smackApp/Controller/ChatVC.swift
@@ -0,0 +1,25 @@
+//
+//  ChatVC.swift
+//  smackApp
+//
+//  Created by Vu, Huy (Contractor) on 10/8/18.
+//  Copyright © 2018 Vu, Huy. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+    
+
+}
