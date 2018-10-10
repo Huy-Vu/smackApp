049b7508a68129a2dc31e694b9d44b3c722f77d8
Huy Vu <Huy.Vu@searshc.com>
2018-10-09 21:35:43 -0500

    add UI fo LoginVC

---
+++ smackApp/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smackApp
+//
+//  Created by Vu, Huy (Contractor) on 10/9/18.
+//  Copyright © 2018 Vu, Huy. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnTapped(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smackApp/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smackApp
+//
+//  Created by Vu, Huy (Contractor) on 10/9/18.
+//  Copyright © 2018 Vu, Huy. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
